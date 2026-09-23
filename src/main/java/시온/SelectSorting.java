d31bbfe2188f04e24442fd0bc8afec80272e366f
enohs <kmsn01@naver.com>
2026-09-23 21:30:44 +0900

    feat: 출력 추가

--- src/main/java/시온/SelectSorting.java
+++ src/main/java/시온/SelectSorting.java
@@ -5,5 +5,6 @@ public class SelectSorting {
   public static void main(String[] args) {
     System.out.println("예시용 코드입니다.");
     System.out.println("이거 왜 안 되지;;");
+    System.out.println("이제 됩니다.");
   }
 }
